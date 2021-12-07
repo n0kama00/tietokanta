3b551927d670610faf1f88b6cbadd2ebe73fa7a3
Meija Lohiniva <mlohiniva@hotmail.com>
2021-12-07 19:37:56 +0200

    first commit

---
+++ datasets/titlesbygenre.php
@@ -0,0 +1,27 @@
+<?php
+    // Muodosta tietokantayhteys
+    require_once('db.php'); // Ota db.php-tiedosto käyttöön tässä tiedostossa
+    $conn = createDbConnection(); // Kutsutaan db.php-tiedostossa olevaa createDbConnection()-funktiota, joka avaa tietokantayhteden
+    // Muodosta SQL-lause muuttujaan. Tässä vaiheessa tätä ei vielä ajeta kantaan.
+    $sql = "SELECT `primary_title`
+    FROM `titles` INNER JOIN title_genres
+    ON titles.title_id = title_genres.title_id
+    WHERE genre LIKE '%Documentary%'
+    LIMIT 10;";
+    // Tarkistukset yms
+    // Aja kysely kantaan
+    $prepare = $conn->prepare($sql);
+    $prepare->execute();
+    // Tallenna vastaus muuttujaan
+    $rows = $prepare->fetchAll();
+    // Tulosta otsikko
+    $html = '<h1>Documentaries</h1>';
+    // Avaa ul-elementti
+    $html .= '<ul>';
+    // Looppaa tietokannasta saadut rivit läpi
+    foreach($rows as $row) {
+        // Tulosta jokaiselle riville li-elementti
+        $html .= '<li>' . $row['primary_title'] . '</li>';
+    }
+    $html .= '</ul>';
+    echo $html;
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    require_once('utilities.php');
+    // Hakukriteerit
+    $html = "<h2>Criteria</h2>";
+    $html .= '<form action="GET">';
+    // Alue-pudotusvalikko
+    $html .= createRegionDropDown();
+    // Genre-pudotusvalikko
+    $html .= createGenreDropDown();
+    // Looppaa läpi tiedostot datasets-hakemistosta
+    $path = 'datasets';
+    if ($handle = opendir($path)) {
+        while (false !== ($file = readdir($handle))) {
+            if ('.' === $file) continue;
+            if ('..' === $file) continue;
+
+            $html .= '<div><input type="submit" value="' . basename($file, ".php") . '" formaction="' . $path . "/" . $file . '"></div>';
+        }
+        closedir($handle);
+    }
+    $html .= '</form>';
+    // Luo painike, joka lähettää lomakkeen käsiteltävänä olevalle tiedostolle
+    echo $html;
